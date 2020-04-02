6e926a125756441b579cb0cc92cd3781dfc6b6c9
aleks <aleks.lisutta@gmail.com>
2020-04-03 00:25:27 +0300

    initial persistent object and task impl, also branch test

---
+++ Backend/BusinessLayer/Task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+    class Task : IPersistentObject
+    {
+        private int ID;
+        private string title;
+        private string desc;
+        private DateTime due;
+        private DateTime creation;
+        private string email;
+
+        internal Task(int ID, string title, string desc, DateTime due, string email) 
+        {
+            this.ID = ID;
+            this.title = title;
+            this.desc = desc;
+            this.due = due;
+            this.email = email;
+            creation = DateTime.Now;
+        }
+
+        public void ToDalObject() { throw new NotImplementedException(); }
+
+        void editTitle(string title)
+        {
+            this.title = title;
+        }
+        void editDesc(string desc)
+        {
+            this.desc = desc;
+        }
+        void editDue(DateTime due)
+        {
+            this.due = due;
+        }
+
+    }
+}
