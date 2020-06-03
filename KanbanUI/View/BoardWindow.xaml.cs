5f43e2a7815b3194e683c5b3c0fe1969d3c2bf79
yakov elmaleh <yakovelm@post.bgu.ac.il>
2020-06-03 23:27:29 +0300

    UI

---
+++ KanbanUI/View/BoardWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using KanbanUI.Model;
+
+namespace KanbanUI.View
+{
+    /// <summary>
+    /// Interaction logic for BoardWindow.xaml
+    /// </summary>
+    public partial class BoardWindow : Window
+    {
+        private UserModel UM;
+        public BoardWindow(UserModel User)
+        {
+            InitializeComponent();
+            UM = User;
+            DataContext = UM;
+        }
+    }
+}
---
+++ KanbanUI/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using KanbanUI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanUI.ViewModel
+{
+    class RegisterViewModel: NotifiableModelObject
+    {
+        public RegisterViewModel(BackendController controller): base(controller) { }
+    }
+}
